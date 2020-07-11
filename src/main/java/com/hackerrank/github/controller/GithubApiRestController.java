08f9302b1629e8709589a5ec29b730bcca8c8bd4
Maclean Maurice Pinto <macleanmaurice.pinto@boeing.com>
2020-07-11 04:28:15 -0700

    BPTT-123 Update sprin boot

--- src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
+++ src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
@@ -1,7 +1,22 @@
 package com.hackerrank.github.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hackerrank.github.model.Event;
+import com.hackerrank.github.repository.EventRepository;
 
 @RestController
 public class GithubApiRestController {
+	@Autowired
+	private EventRepository eventRepository;
+
+	@PostMapping("/events")
+	public HttpStatus createEvent(@RequestBody Event event) {
+		eventRepository.save(event);
+		return HttpStatus.CREATED;
+	}
 }
--- src/main/java/com/hackerrank/github/repository/EventRepository.java
+++ src/main/java/com/hackerrank/github/repository/EventRepository.java
@@ -1,4 +1,10 @@
 package com.hackerrank.github.repository;
 
-public interface EventRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hackerrank.github.model.Event;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Long> {
 }
