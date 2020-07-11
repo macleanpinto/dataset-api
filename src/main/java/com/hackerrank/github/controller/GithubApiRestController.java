ee5e5658554f765f852f0f7cbe5f206aa0b5f7c8
macleanpinto <macleanmauricepinto@gmail.com>
2020-07-11 12:14:16 +0000

    delete all events

--- src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
+++ src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
@@ -3,6 +3,7 @@ package com.hackerrank.github.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,15 @@ public class GithubApiRestController {
     private EventRepository eventRepository;
 
     @PostMapping("/events")
-    public ResponseEntity<HttpStatus> createEvent(@RequestBody Event event) {
-        if(eventRepository.findById(event.getId()).isPresent()) return ResponseEntity.ok().body(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<HttpStatus> addNewEvents(@RequestBody Event event) {
+        if(eventRepository.findById(event.getId()).isPresent()) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         eventRepository.save(event);
-        return ResponseEntity.ok().body(HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/erase")
+    public ResponseEntity<HttpStatus> eraseAllEvents(){
+        eventRepository.deleteAll();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
