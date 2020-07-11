dece1e093d7a1b32cb03dd13bc2c3aadad5b0f0f
macleanpinto <macleanmauricepinto@gmail.com>
2020-07-11 15:54:52 +0000

    Return all events

--- src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
+++ src/main/java/com/hackerrank/github/controller/GithubApiRestController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import com.hackerrank.github.model.Event;
 import com.hackerrank.github.repository.EventRepository;
@@ -18,14 +21,20 @@ public class GithubApiRestController {
 
     @PostMapping("/events")
     public ResponseEntity<HttpStatus> addNewEvents(@RequestBody Event event) {
-        if(eventRepository.findById(event.getId()).isPresent()) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        if (eventRepository.findById(event.getId()).isPresent())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         eventRepository.save(event);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/erase")
-    public ResponseEntity<HttpStatus> eraseAllEvents(){
+    public ResponseEntity<HttpStatus> eraseAllEvents() {
         eventRepository.deleteAll();
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<List<Event>> fetchAllEvents() {
+        return ResponseEntity.status(HttpStatus.OK).body(eventRepository.findAllByEventByIdAsc());
     }
 }
--- src/main/java/com/hackerrank/github/repository/EventRepository.java
+++ src/main/java/com/hackerrank/github/repository/EventRepository.java
@@ -3,8 +3,12 @@ package com.hackerrank.github.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.hackerrank.github.model.Event;
 
 @Repository
 public interface EventRepository extends CrudRepository<Event, Long> {
+
+    public List<Event> findAllByEventByIdAsc();
 }
